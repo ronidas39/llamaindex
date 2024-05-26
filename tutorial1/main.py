a57e5cdfa27ee5039a452f85fda913b8169ebf39
ronidas39 <ronidas041987@gmail.com>
2024-05-26 15:51:59 +0530

    tutorial1

--- tutorial1/main.py
+++ tutorial1/main.py
@@ -1,7 +1,6 @@
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
-
-documents = SimpleDirectoryReader("data").load_data()
-index = VectorStoreIndex.from_documents(documents)
-query_engine = index.as_query_engine()
-response = query_engine.query("Who is the author here , can you tell me his name?")
+from llama_index.core import VectorStoreIndex,SimpleDirectoryReader
+documents=SimpleDirectoryReader("data").load_data()
+index=VectorStoreIndex.from_documents(documents)
+query_engine=index.as_query_engine()
+response=query_engine.query("give some information on the career of the author")
 print(response)
